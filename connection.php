555730d2b85a91613f9e3833d8441e9eff74465c
1289dhrupal <31788924+1289dhrupal@users.noreply.github.com>
2020-04-08 22:22:41 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php
-  $mysqli = new mysqli("localhost", "root", "", "abcd");
+  $mysqli = new mysqli("us-cdbr-iron-east-01.cleardb.net", "bc030cbda9a312", "898f0c83", "heroku_9cbad83aa642126");
 
   if(! $mysqli ) {
       die('Could not connect: ' . mysqli_error());
